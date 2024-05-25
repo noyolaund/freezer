328333e5d3e149763100abffe957df17d55c84dc
noyolaund <noyolaund@gmail.com>
2024-05-24 18:45:43 -0600

    Thingsboard changes were added

--- therm_python.py
+++ therm_python.py
@@ -2,6 +2,7 @@ import os
 import glob
 import time
 import RPi.GPIO as GPIO
+from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT)
@@ -12,6 +13,9 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+telemetry = {"temperature": 41.9, "enabled": False, "currentFirmwareVersion": "v1.2.2"}
+client = TBDeviceMqttClient("thingsboard.cloud", username="2o52a4j2x8g1olcqi43e")
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -30,13 +34,21 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
+client.connect()
+
 while True:
-        actual_temp = read_temp()
-        print("Temp is: ", actual_temp)
-        time.sleep(1)
-        if actual_temp > 19:
-                GPIO.output(16, GPIO.HIGH)
-                print("Compressor is ON")
-        else:
-                GPIO.output(16, GPIO.LOW)
-                print("Compressor is OFF")
+    actual_temp = read_temp()
+    print("Temp is: ", actual_temp)
+    time.sleep(1)
+    telemetry["temperature"] = actual_temp
+    client.send_telemetry(telemetry)
+    result = client.send_telemetry(telemetry)
+    success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+    if actual_temp > 19:
+        GPIO.output(16, GPIO.HIGH)
+        print("Compressor is ON")
+    else:
+        GPIO.output(16, GPIO.LOW)
+        print("Compressor is OFF")
+
+client.disconnect()
